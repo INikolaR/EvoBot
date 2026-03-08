8f6c932a43f04925258f37737a45e910b0360a76
= <=>
2026-03-08 21:03:58 +0300

    fix telegram bot output

--- telegram_bot/controllers/telegram_bot_controller.py
+++ telegram_bot/controllers/telegram_bot_controller.py
@@ -12,23 +12,26 @@ class TelegramBotController:
 
     def setup_handlers(self):
         self.app.add_handler(CommandHandler("start", self.start))
-        self.app.add_handler(CommandHandler("help", self.help_command))
+        self.app.add_handler(CommandHandler("help", self.help))
         self.app.add_handler(CommandHandler("reset", self.reset))
         self.app.add_handler(CommandHandler("about", self.about))
         self.app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, self.handle_message))
-
+        self.app.add_handler(CommandHandler("unknown", self.unknown))
     async def start(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-        await update.message.reply_text("Привет! Напишите ваш вопрос")
+        await update.message.reply_text("Привет! Напишите ваш вопрос.")
 
     async def reset(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         await update.message.reply_text("Контекст сброшен, начинаем с чистого листа!")
 
     async def about(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-        await update.message.reply_text("Я - ИИ-консультант по настольной игре «Эволюция»...")
+        await update.message.reply_text("Я - ИИ-консультант по настольной игре \"Эволюция\", разработаной Дмитрием Кнорре в 2010 году. Из-за большого объёма игровых правил у игроков часто возникают вопросы, для ответа на которые нужно перечитывать правила от начала до конца или даже обращаться к помощи других игроков на специальных форумах. Со мной же в этом нет необходимости: я с радостью предоставлю вам ответ на любой вопрос по игре меньше, чем за минуту!")
 
-    async def help_command(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-        await update.message.reply_text("Отправьте ваш вопрос одним сообщением...")
+    async def help(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        await update.message.reply_text("Отправьте ваш вопрос одним сообщением, и я на него отвечу! Для сброса контекста используйте команду /reset, для вывода дополнительной информации обо мне - команду /about. По команде /help выводится это сообщение.")
 
+    async def unknown(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        await update.message.reply_text("Извините, я не понимаю эту команду. Введите /help для получения подсказки.")
+    
     async def handle_message(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         text = update.message.text
         user_id = update.effective_user.id
