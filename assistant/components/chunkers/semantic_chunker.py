f19f4a11ebf3187c4ea332296d14c0fea8f56fef
= <=>
2026-04-05 18:46:27 +0300

    fix semantic chunker bug

--- assistant/components/chunkers/semantic_chunker.py
+++ assistant/components/chunkers/semantic_chunker.py
@@ -1,12 +1,13 @@
+from langchain_huggingface import HuggingFaceEmbeddings
 from assistant.core.chunker import Chunker
 from typing import List
-from langchain_text_splitters import SemanticChunker
+from langchain_experimental.text_splitter import SemanticChunker
 from langchain_core.embeddings import Embeddings
 
 class EmbeddingSemanticChunker(Chunker):
-    def __init__(self, embeddings: Embeddings, threshold_type: str = "percentile", threshold_amount: float = 95):
+    def __init__(self, model_name: str, threshold_type: str = "percentile", threshold_amount: float = 95):
         self.splitter = SemanticChunker(
-            embeddings=embeddings,
+            embeddings=HuggingFaceEmbeddings(model_name=model_name),
             breakpoint_threshold_type=threshold_type,
             breakpoint_threshold_amount=threshold_amount
         )
