a1a2530b53563bf14fdcd5844b5b753671d086e5
= <=>
2026-04-23 22:01:25 +0300

    add multi-file processing

--- assistant/components/generators/default_generator.py
+++ assistant/components/generators/default_generator.py
@@ -4,5 +4,5 @@ class DefaultGenerator(Generator):
     def __init__(self):
         pass
 
-    def __call__(self, input_data: str) -> str:
-        return input_data.to_string()
+    def __call__(self, input_data) -> str:
+        return str(input_data)
