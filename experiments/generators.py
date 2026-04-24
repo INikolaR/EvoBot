cd1c40cdc7e35df08d4a6021d8ee53f1ae8dcf8a
= <=>
2026-04-24 10:33:41 +0300

    separate generators experiments

--- experiments/generators.py
+++ experiments/generators.py
@@ -1,6 +1,7 @@
 from assistant.components.generators.hf_model_generator import HFModelGenerator
 import json
 import torch
+import sys
 
 assert torch.cuda.is_available(), "No CUDA provided!"
 
@@ -13,44 +14,43 @@ system_instruction = """Ты - консультант по серии насто
                     
 Важно: ответь только на заданный вопрос. Не задавай встречных вопросов, не предлагай продолжить диалог и не генерируй новые темы."""
 
-generator_names = ["Qwen/Qwen2.5-3B-Instruct", "Qwen/Qwen2.5-7B-Instruct", "Qwen/Qwen2.5-14B-Instruct", "Qwen/Qwen2.5-32B-Instruct"]
-for generator_name in generator_names[:2]:
+generator_name = sys.argv[1]
 
-    torch.cuda.empty_cache()
-    model = HFModelGenerator(model_name=generator_name)
+torch.cuda.empty_cache()
+model = HFModelGenerator(model_name=generator_name)
 
-    batch_size = 4
-    json_results = []
+batch_size = 4
+json_results = []
 
-    for i in range(0, len(elements), batch_size):
-        elem_batch = elements[i:min(i+batch_size, len(elements))]
+for i in range(0, len(elements), batch_size):
+    elem_batch = elements[i:min(i+batch_size, len(elements))]
 
-        questions = [elem["question"] for elem in elem_batch]
+    questions = [elem["question"] for elem in elem_batch]
 
-        system_content = """Ты - консультант по серии настольных игр "Эволюция". Ты должен помочь пользователю понять игровые правила, ответив на русском языке на его вопрос.
+    system_content = """Ты - консультант по серии настольных игр "Эволюция". Ты должен помочь пользователю понять игровые правила, ответив на русском языке на его вопрос.
 
 Суть игры заключается в том, чтобы создать наиболее жизнеспособную популяцию животных.
-                    
+                
 Важно: ответь только на заданный вопрос. Не задавай встречных вопросов, не предлагай продолжить диалог и не генерируй новые темы. Для ответа на вопрос используй предоставленный контекст.
 
 Важно: ты отвечаешь ТОЛЬКО на русском языке. Запрещено использовать китайские, английские или иные иностранные слова, символы или фразы. Все ответы должны содержать только кириллицу, цифры и базовые знаки препинания."""
 
-        prompt_batch = [[
-            {"role": "system", "content": system_content},
-            {"role": "user", "content": f"""
+    prompt_batch = [[
+        {"role": "system", "content": system_content},
+        {"role": "user", "content": f"""
 Контекст:
 {elem["paragraph"]}
 
 Вопрос:
 {elem["question"]}
 """}
-        ] for elem in elem_batch]
+    ] for elem in elem_batch]
 
-        model_answer_batch = model(prompt_batch, temperature=0.0)
+    model_answer_batch = model(prompt_batch, temperature=0.0)
 
-        json_result_batch = [{"question" : elem["question"], "model_answer" : model_answer, "reference_answer" : elem["answer"]} for elem, model_answer in zip(elem_batch, model_answer_batch)]
+    json_result_batch = [{"question" : elem["question"], "model_answer" : model_answer, "reference_answer" : elem["answer"]} for elem, model_answer in zip(elem_batch, model_answer_batch)]
 
-        json_results.extend(json_result_batch)
+    json_results.extend(json_result_batch)
 
-    with open(f"experiment_results/generator_output_model_{generator_name.split('/')[-1]}.txt", "w", encoding="utf-8") as f:
-        f.write(json.dumps(json_results, ensure_ascii=False, indent=4))
+with open(f"experiment_results/generator_output_model_{generator_name.split('/')[-1]}.txt", "w", encoding="utf-8") as f:
+    f.write(json.dumps(json_results, ensure_ascii=False, indent=4))
